b3ef6ad878e8aec3d5f33f631a827e3a69ee4605
DevPCW <rorndksk123@naver.com>
2022-12-01 21:55:42 +0900

    저녁웹개발: 저장소/ 로그인창 2022-12-01_changwoo

--- src/main/java/com/study/servlet/form/Form1.java
+++ src/main/java/com/study/servlet/form/Form1.java
@@ -16,7 +16,10 @@ public class Form1 extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/form1.html").forward(request, response);
+		System.out.println("form1 호출!!!");
+		System.out.println(request.getAttribute("name")); // request 저장소에 들어가있는거 꺼내겠다는 의미
+		request.getRequestDispatcher("/WEB-INF/name.jsp").forward(request, response);
+//		request.getRequestDispatcher("/WEB-INF/form1.html").forward(request, response);
 	}
 
 
